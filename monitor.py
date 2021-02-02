079efab0acc6e1679735b4ae6f7399ac345433f7
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-02-01 16:23:26 -0800

    byte convert

--- monitor.py
+++ monitor.py
@@ -13,4 +13,21 @@ def Bandwidth():
         value = newValue
         return(convertToBits(value))
 def convertToBits(number):
-    
+# 1 kilobyte = 1024 bytes
+#1 Byte = 8 Bit 1 Kilobyte = 1,024 Bytes
+#1 Megabyte = 1,048,576 Bytes 
+#1 Gigabyte = 1,073,741,824 Bytes
+
+    if number > 1024*1024*1024:
+        number = number/(1024*1024*)*8
+        number = str(number) + 'Gigabytes'
+    elif number > 1024*1024:
+        number = number/(1024*1024)*8
+        number = str(number) + 'Megabytes'
+    elif number = 1024:
+        number = number/1024*8
+        number = str(number) + 'Kilobytes'
+    else number = str(number) + 'Bytes'
+
+def printStat(val):
+    print(convertToBits(val))
